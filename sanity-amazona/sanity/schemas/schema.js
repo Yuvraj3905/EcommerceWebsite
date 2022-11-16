29d97a4cfbd2079576b9e61aa9f46dc7ab8f608e
yuvraj3905 <yuvraj202001@gmail.com>
2022-11-16 23:33:10 +0530

    Update schema.js

--- sanity-amazona/sanity/schemas/schema.js
+++ sanity-amazona/sanity/schemas/schema.js
@@ -1,7 +1,7 @@
 // First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator';
 
-// Then import schema types from any plugins that might expose them
+// import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 import product from './product';
 
