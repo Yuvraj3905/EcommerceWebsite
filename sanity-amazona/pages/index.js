a9bdf839c7a107eb552a96bee1afcfa1aa0ed41e
yuvraj3905 <yuvraj202001@gmail.com>
2022-11-18 18:46:40 +0530

    Update index.js

--- sanity-amazona/pages/index.js
+++ sanity-amazona/pages/index.js
@@ -1,7 +1,17 @@
 import { Typography } from '@mui/material';
 import Head from 'next/head';
+import { useEffect } from 'react';
 import Layout from '../components/Layout';
 
 export default function Home() {
+  useEffect(() => {
+    const fetchData=async()=>{
+      try{
+        const products =await client.fetch();
+      } catch(err){
+
+      }
+    }
+  })
   return <Layout>List Products</Layout>;
 }
